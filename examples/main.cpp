15ef8268153a7752e394f8fa6d837cd7832918f2
Kyle Price <kyleprice@yahoo.com>
2020-08-21 16:34:20 -0400

    formatting cleanup

--- examples/main.cpp
+++ examples/main.cpp
@@ -18,36 +18,33 @@ std::vector<std::vector<std::string>> stringPools = {
     {"test", "thread", "safety", "by", "single"},
     {"placing", "a", "ton", "of", "random"},
     {"strings", "of", "text", "this", "filler"},
-    {"into", "a", "small", "ring", "buffer"}};
+    {"into", "a", "small", "ring", "buffer"}
+};
 
-void threadFunc(const std::vector<std::string> &toAdd, Pricetec::TSRingBufferDecorator<std::string> *buf)
-{
+void threadFunc(const std::vector<std::string> &toAdd, 
+                Pricetec::TSRingBufferDecorator<std::string> *buf) {
     auto randIdx = rand() % toAdd.size();
 
     buf->add(toAdd[randIdx]);
 }
 
-int main(int argc, const char *argv[])
-{
+int main(int argc, const char *argv[]) {
     const auto NUM_THREADS = 10000;
     std::vector<std::thread> threadPool;
     Pricetec::TSRingBufferDecorator stringRing(new Pricetec::SimpleRingBuffer<std::string>(5));
 
     srand(time(nullptr));
 
-    for (int i = 0; i < NUM_THREADS; i++)
-    {
+    for (int i = 0; i < NUM_THREADS; i++) {
         auto stringsIdx = rand() % stringPools.size();
         threadPool.push_back(std::thread(threadFunc, stringPools[stringsIdx], &stringRing));
     }
 
-    for (auto &t : threadPool)
-    {
+    for (auto &t : threadPool) {
         t.join();
     }
 
-    for (const auto &val : stringRing)
-    {
+    for (const auto &val : stringRing) {
         std::cout << val << " ";
     }
 
